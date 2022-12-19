2ca361863456f158483158373e8dbe590eb19154
Jamie Magee <jamie.magee@gmail.com>
2022-12-18 22:00:21 -0800

    fix dev systempackages

--- hosts/common/optional/dev.nix
+++ hosts/common/optional/dev.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    (with dotnetCorePackages;
-      combinePackages [
-        sdk_3_1
-        sdk_6_0
-        sdk_7_0
-      ])
-    nodejs-19_x
-    (with nodePackages; [yarn])
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      (with dotnetCorePackages;
+        combinePackages [
+          sdk_3_1
+          sdk_6_0
+          sdk_7_0
+        ])
+      nodejs-19_x
+    ]
+    ++ (with nodePackages; [yarn]);
 }
