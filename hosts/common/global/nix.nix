83317ba9aa2261750bd9e417187ef852675870b1
Jamie Magee <jamie.magee@gmail.com>
2022-12-29 14:20:51 -0800

    (nix): show diff on nixos rebuild

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -26,4 +26,15 @@
     # Very useful when using legacy commands
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
   };
+
+  system.activationScripts.system-report-changes = ''
+    PATH=$PATH:${lib.makeBinPath [pkgs.nvd pkgs.nix]}
+    # Disable nvd if there are lesser than 2 profiles in the system.
+    if [ $(ls -d1v /nix/var/nix/profiles/system-*-link 2>/dev/null | wc -l) -lt 2 ];
+    then
+        echo "Skipping reporting changes..."
+    else
+        nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -2)
+    fi
+  '';
 }
