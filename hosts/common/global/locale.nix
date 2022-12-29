c183e54fb73152692979e76e493ea946810c6d79
Jamie Magee <jamie.magee@gmail.com>
2022-12-29 14:30:09 -0800

    (locale): no need to set `LC_TIME` explicitly

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,9 +1,6 @@
 {lib, ...}: {
   i18n = {
     defaultLocale = lib.mkDefault "en_GB.UTF-8";
-    extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "en_GB.UTF-8";
-    };
     supportedLocales = lib.mkDefault [
       "en_GB.UTF-8/UTF-8"
     ];
