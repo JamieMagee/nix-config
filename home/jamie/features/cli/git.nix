f4e58b277650ab5640163e3f17d7ff928929339c
Jamie Magee <jamie.magee@gmail.com>
2023-01-07 23:08:56 -0800

    (git): sign commits by default

--- home/jamie/features/cli/git.nix
+++ home/jamie/features/cli/git.nix
@@ -4,16 +4,16 @@
     package = pkgs.gitAndTools.gitFull;
     userName = "Jamie Magee";
     userEmail = "jamie.magee@gmail.com";
-    # signing = {
-    #   key = "key::sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILDsTqh0sg8miK75yvFofimTiktt9WTcqSq8FXSMMhtgAAAABHNzaDo=";
-    #   signByDefault = true;
-    # };
+    signing = {
+      key = "key::sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILDsTqh0sg8miK75yvFofimTiktt9WTcqSq8FXSMMhtgAAAABHNzaDo=";
+      signByDefault = true;
+    };
     difftastic = {
       enable = true;
     };
     extraConfig = {
-      # commit.gpgsign = true;
-      # gpg.format = "ssh";
+      commit.gpgsign = true;
+      gpg.format = "ssh";
       feature.manyFiles = true;
       init.defaultBranch = "main";
     };
