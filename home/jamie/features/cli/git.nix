b031337dcf8ad9e365ab5d8c13b23715d826d5eb
Jamie Magee <jamie.magee@gmail.com>
2023-01-08 15:20:12 -0800

    git: some more configuration

--- home/jamie/features/cli/git.nix
+++ home/jamie/features/cli/git.nix
@@ -4,6 +4,20 @@
     package = pkgs.gitAndTools.gitFull;
     userName = "Jamie Magee";
     userEmail = "jamie.magee@gmail.com";
+    aliases = {
+      co = "checkout";
+      cob = "checkout -b";
+
+      tags = "tag -l";
+      branches = "branch -a";
+      remotes = "remote -v";
+
+      amend = "commit -a --amend";
+
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
+      whoami = "!sh -c 'echo \"$(git config --get user.name) <$(git config --get user.email)>\"'";
+    };
     signing = {
       key = "key::sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEfboykpOQU4syTfATJPL+CICYyZdVXOROU2O4iLmmA9AAAABHNzaDo= ";
       signByDefault = true;
@@ -12,10 +26,33 @@
       enable = true;
     };
     extraConfig = {
-      commit.gpgsign = true;
-      gpg.format = "ssh";
-      feature.manyFiles = true;
-      init.defaultBranch = "main";
+      branch = {
+        autosetuprebase = "always";
+      };
+      core = {
+        autocrlf = "input";
+        # https://git-scm.com/docs/git-config#Documentation/git-config.txt-corefsmonitor
+        fsmonitor = true;
+        safecrlf = false;
+        untrackedCache = true;
+      };
+      feature = {
+        # https://git-scm.com/docs/git-config#Documentation/git-config.txt-featuremanyFiles
+        manyFiles = true;
+      };
+      format = {
+        signOff = true;
+      };
+      gpg = {
+        format = "ssh";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      push = {
+        # https://git-scm.com/docs/git-config#Documentation/git-config.txt-pushdefaultgit
+        default = "current";
+      };
     };
     lfs = {
       enable = true;
