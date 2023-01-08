1b5c03d3ddfc84a87a00457a16cbabadc2d41cfc
Jamie Magee <jamie.magee@gmail.com>
2023-01-07 23:15:36 -0800

    (ssh): define authorizedKeys at user level

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -5,12 +5,6 @@
       ports = [2222];
       permitRootLogin = "no";
       passwordAuthentication = false;
-      authorizedKeysFiles = [
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILcVlek7bNIbXecoGz7IvqZWFd1FQJReULawc2yFNdXtAAAABHNzaDo="
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJzX8m5ulb8Yh+mLQL1vqT1d9k0/Kjo6GvJJSRQxK9dxAAAABHNzaDo="
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH9nPL5Z2gsdclYU43EjADaN4JmXMMTijxcv4cv7T7KtAAAABHNzaDo="
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEfboykpOQU4syTfATJPL+CICYyZdVXOROU2O4iLmmA9AAAABHNzaDo="
-      ];
     };
   };
   programs.ssh.startAgent = true;
