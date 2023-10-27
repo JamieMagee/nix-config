53131c687d57d7c5fe6c9890c596f46f9394b351
Jamie Magee <jamie.magee@gmail.com>
2023-10-26 22:35:58 -0700

    esphome: add reverse proxy

--- hosts/rpi/services/home-assistant/esphome.nix
+++ hosts/rpi/services/home-assistant/esphome.nix
@@ -8,4 +8,10 @@
   services.esphome = {
     enable = true;
   };
+
+  services.caddy.virtualHosts."alfred.tailnet-0b15.ts.net" = {
+    extraConfig = ''
+      reverse_proxy /esphome* http://[::1]:6052
+    '';
+  };
 }
