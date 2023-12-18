dbfe3e5f262238f5ec87fc517f732f94ba4768cb
Jamie Magee <jamie.magee@gmail.com>
2023-12-18 10:43:07 -0800

    git: add workaround for gh cli issue

--- home/jamie/features/cli/git.nix
+++ home/jamie/features/cli/git.nix
@@ -76,5 +76,9 @@
     gitCredentialHelper = {
       enable = true;
     };
+    settings = {
+      # Workaround for https://github.com/nix-community/home-manager/issues/4744
+      version = 1;
+    };
   };
 }
