dbaadd905fcaf99cac923d41ff7161bdd42f9fc5
Jamie Magee <jamie.magee@gmail.com>
2024-02-09 20:48:16 -0800

    migrate to mise instead of nixos for ruby

--- home/jamie/features/cli/ruby.nix
+++ home/jamie/features/cli/ruby.nix
@@ -1,5 +1,19 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    ruby
-  ];
+  # pkgs.ruby doesn't play well with psych
+  # Install ruby with mise instead
+  programs = {
+    mise = {
+      enable = true;
+      globalConfig = {
+        tools = {
+          ruby = "3.1.4";
+        };
+      };
+    };
+    fish = {
+      shellInit = ''
+        fish_add_path ~/.local/share/mise/shims
+      '';
+    };
+  };
 }
