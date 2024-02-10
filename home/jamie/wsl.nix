77e6f3167a4d1a1474a830ce7a7e713c3f04dbd7
Jamie Magee <jamie.magee@gmail.com>
2024-02-09 20:47:59 -0800

    add wslu

--- home/jamie/wsl.nix
+++ home/jamie/wsl.nix
@@ -1,8 +1,16 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     ./global
 
     ./features/cli
     ./features/cli/ruby.nix
+  ];
+
+  home.packages = with pkgs; [
+    wslu
   ];
 }
