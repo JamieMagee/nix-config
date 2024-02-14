ad4949ac08671caf6d3ba9a8ec03c2759da1a21d
Jamie Magee <jamie.magee@gmail.com>
2024-02-13 16:11:16 -0800

    wsl: add git credential manager

--- home/jamie/wsl.nix
+++ home/jamie/wsl.nix
@@ -10,9 +10,19 @@
     ./features/cli/ruby.nix
   ];
 
-  home.packages = with pkgs; [
-    nodejs_21
-    wslu
-  ]
-  ++ (with nodePackages_latest; [pnpm yarn]);
+  home.packages = with pkgs;
+    [
+      nodejs_21
+      wslu
+    ]
+    ++ (with nodePackages_latest; [pnpm yarn]);
+
+  programs.git.extraConfig = {
+    credential = {
+      "https://dev.azure.com" = {
+        helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+        useHttpPath = true;
+      };
+    };
+  };
 }
