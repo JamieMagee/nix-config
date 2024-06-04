758468b8d90c479cc3de0b6756f8b7ad8b8a3d12
Jamie Magee <jamie.magee@gmail.com>
2024-06-03 21:45:21 -0700

    audit: add another audit rule

--- hosts/common/optional/audit.nix
+++ hosts/common/optional/audit.nix
@@ -4,6 +4,7 @@
       enable = true;
       rules = [
         "-w /dev/ttyUSB0 -p rwxa"
+        "-w /dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_a61ce607b596ed11b11cc498a7669f5d-if00-port0 -p rwxa"
       ];
     };
     auditd.enable = true;
