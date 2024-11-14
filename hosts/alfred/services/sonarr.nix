7fe763553d288ba824da1b3b80fad21fac4f09dd
Jamie Magee <jamie.magee@gmail.com>
2024-11-14 09:36:39 -0800

    sonarr: use ipv6 internally

--- hosts/alfred/services/sonarr.nix
+++ hosts/alfred/services/sonarr.nix
@@ -5,7 +5,7 @@
 
   services.caddy.virtualHosts."alfred.tailnet-0b15.ts.net" = {
     extraConfig = ''
-      reverse_proxy /sonarr* http://127.0.0.1:8989
+      reverse_proxy /sonarr* http://[::1]:8989
     '';
   };
 }
