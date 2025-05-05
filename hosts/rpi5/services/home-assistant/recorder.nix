c6b666a850100b9d39892d7e95eeeaae3f6208d8
Jamie Magee <jamie.magee@gmail.com>
2025-05-04 18:04:18 -0700

    home-assistant: exclude inovelli from recorder

--- hosts/rpi5/services/home-assistant/recorder.nix
+++ hosts/rpi5/services/home-assistant/recorder.nix
@@ -1,3 +1,19 @@
 {
-  services.home-assistant.config.recorder = { };
+  services.home-assistant.config = {
+    logger = {
+      logs = {
+        homeassistant.components.recorder = "debug";
+      };
+    };
+    recorder = {
+      exclude = {
+        entity_globs = [
+          "button.*_switch_*"
+          "number.*_switch_*"
+          "select.*_switch_*"
+          "sensor.*_switch_*"
+        ];
+      };
+    };
+  };
 }
