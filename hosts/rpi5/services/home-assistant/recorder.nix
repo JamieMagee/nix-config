3fa6e28d9e02d6330e1a04f31749426f6291d511
Jamie Magee <jamie.magee@gmail.com>
2025-05-05 19:55:04 -0700

    home-assistant: remove recorder logger

--- hosts/rpi5/services/home-assistant/recorder.nix
+++ hosts/rpi5/services/home-assistant/recorder.nix
@@ -1,19 +1,12 @@
 {
-  services.home-assistant.config = {
-    logger = {
-      logs = {
-        homeassistant.components.recorder = "debug";
-      };
-    };
-    recorder = {
-      exclude = {
-        entity_globs = [
-          "button.*_switch_*"
-          "number.*_switch_*"
-          "select.*_switch_*"
-          "sensor.*_switch_*"
-        ];
-      };
+  services.home-assistant.config.recorder = {
+    exclude = {
+      entity_globs = [
+        "button.*_switch_*"
+        "number.*_switch_*"
+        "select.*_switch_*"
+        "sensor.*_switch_*"
+      ];
     };
   };
 }
