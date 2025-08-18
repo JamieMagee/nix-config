0e827b3ac1f073dcfd26521db991eb03d20f4bd2
Jamie Magee <jamie.magee@gmail.com>
2025-08-18 14:35:45 -0700

    zellij: switch to dracula theme

--- home/jamie/features/cli/default.nix
+++ home/jamie/features/cli/default.nix
@@ -62,19 +62,21 @@
       attachExistingSession = true;
       exitShellOnExit = true;
       settings = {
-        theme = "nord";
-        themes.nord = {
-          fg = "#D8DEE9";
-          bg = "#2E3440";
-          black = "#3B4252";
-          red = "#BF616A";
-          green = "#A3BE8C";
-          yellow = "#EBCB8B";
-          blue = "#81A1C1";
-          magenta = "#B48EAD";
-          cyan = "#88C0D0";
-          white = "#E5E9F0";
-          orange = "#D08770";
+        theme = "dracula";
+        themes = {
+          nord = {
+            fg = "#D8DEE9";
+            bg = "#2E3440";
+            black = "#3B4252";
+            red = "#BF616A";
+            green = "#A3BE8C";
+            yellow = "#EBCB8B";
+            blue = "#81A1C1";
+            magenta = "#B48EAD";
+            cyan = "#88C0D0";
+            white = "#E5E9F0";
+            orange = "#D08770";
+          };
         };
         default_layout = "compact";
         pane_frames = false;
