a8ad62ffaffa6a52527854a4f26bfdab42a7cd3a
Jamie Magee <jamie.magee@gmail.com>
2025-08-18 14:38:50 -0700

    starship: use dracula theme

--- home/jamie/features/cli/starship.nix
+++ home/jamie/features/cli/starship.nix
@@ -1,5 +1,16 @@
 {
   programs.starship = {
     enable = true;
+    settings = {
+      cmd_duration.style = "bold #f1fa8c";
+      directory.style = "bold #50fa7b";
+      hostname.style = "bold #ff5555";
+      git_branch.style = "bold #ff79c6";
+      git_status.style = "bold #ff5555";
+      username = {
+        format = "[$user]($style) on ";
+        style_user = "bold #bd93f9";
+      };
+    };
   };
 }
