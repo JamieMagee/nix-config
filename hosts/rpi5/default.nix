6a9cc940e7cf90981cc2a31ba3c8f63a6370080c
Jamie Magee <jamie.magee@gmail.com>
2025-09-22 19:16:45 -0700

    rpi5: temporarily disable ip configuration

--- hosts/rpi5/default.nix
+++ hosts/rpi5/default.nix
@@ -14,19 +14,19 @@
 
   networking = {
     hostName = "rpi5";
-    defaultGateway = {
-      address = "192.168.1.1";
-      interface = "end0";
-    };
-    interfaces.end0 = {
-      wakeOnLan.enable = true;
-      ipv4.addresses = [
-        {
-          address = "192.168.1.3";
-          prefixLength = 24;
-        }
-      ];
-    };
+    # defaultGateway = {
+    #   address = "192.168.1.1";
+    #   interface = "end0";
+    # };
+    # interfaces.end0 = {
+    #   wakeOnLan.enable = true;
+    #   ipv4.addresses = [
+    #     {
+    #       address = "192.168.1.3";
+    #       prefixLength = 24;
+    #     }
+    #   ];
+    # };
   };
 
   raspberry-pi-nix.libcamera-overlay.enable = false;
