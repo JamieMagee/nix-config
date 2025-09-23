c0988d80bb446fdc0f481c591e41e4db9971819d
Jamie Magee <jamie.magee@gmail.com>
2025-09-22 19:25:09 -0700

    nix: fix nixos cache public key

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -32,7 +32,7 @@
         "https://jamiemagee.cachix.org"
       ];
       trusted-public-keys = [
-        "nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "jamiemagee.cachix.org-1:IzalYx3F8h0uP7EdifGZxqGkTwaQIKXj0i67PuNNYM8="
       ];
