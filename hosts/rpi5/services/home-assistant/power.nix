8114f29181039145d394b3c917615d8baf7a24d4
Jamie Magee <jamie.magee@gmail.com>
2025-10-03 14:55:06 -0700

    home-assistant: correct power readings for lights

--- hosts/rpi5/services/home-assistant/power.nix
+++ hosts/rpi5/services/home-assistant/power.nix
@@ -5,6 +5,19 @@
     ];
 
     config = {
+      sensor = [
+        {
+          platform = "integration";
+          source = "sensor.total_lights_power";
+          name = "Total Lights Energy";
+          unique_id = "total_lights_energy";
+          unit_prefix = "k";
+          unit_time = "h";
+          round = 2;
+          method = "left";
+        }
+      ];
+
       template = [
         {
           sensor = [
@@ -45,45 +58,6 @@
                   {% set total.value = total.value + (states(switch) | float(0)) %}
                 {% endfor %}
                 {{ total.value | round(2) }}
-              '';
-            }
-            {
-              name = "Total Lights Energy";
-              unique_id = "total_lights_energy";
-              unit_of_measurement = "kWh";
-              device_class = "energy";
-              state_class = "total_increasing";
-              state = ''
-                {% set switches = [
-                  'sensor.garage_switch_outdoors_energy',
-                  'sensor.garage_switch_indoors_energy',
-                  'sensor.garage_bathroom_switch_light_energy',
-                  'sensor.garage_hallway_switch_stairs_energy',
-                  'sensor.kitchen_switch_stairs_light_energy',
-                  'sensor.kitchen_switch_kitchen_light_energy',
-                  'sensor.kitchen_switch_living_room_light_energy',
-                  'sensor.living_room_switch_living_room_light_energy',
-                  'sensor.living_room_switch_kitchen_light_energy',
-                  'sensor.living_room_switch_stairs_light_energy',
-                  'sensor.living_room_switch_front_porch_energy',
-                  'sensor.upstairs_switch_hallway_light_1_energy',
-                  'sensor.upstairs_switch_hallway_light_2_energy',
-                  'sensor.upstairs_switch_stairs_light_energy',
-                  'sensor.upstairs_switch_rooftop_energy',
-                  'sensor.bedroom_switch_energy',
-                  'sensor.bedroom_switch_closet_energy',
-                  'sensor.office_switch_energy',
-                  'sensor.bathroom_switch_overhead_1_energy',
-                  'sensor.bathroom_switch_overhead_2_energy',
-                  'sensor.bathroom_switch_vanity_1_energy',
-                  'sensor.bathroom_switch_vanity_2_energy',
-                  'sensor.kitchen_switch_balcony_energy'
-                ] %}
-                {% set total = namespace(value=0) %}
-                {% for switch in switches %}
-                  {% set total.value = total.value + (states(switch) | float(0)) %}
-                {% endfor %}
-                {{ total.value | round(3) }}
               '';
             }
             # Average power per active switch
