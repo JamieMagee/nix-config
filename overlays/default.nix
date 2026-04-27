dd219eef227672b0de31d3fccc6b4224508d64a9
Jamie Magee <jamie.magee@gmail.com>
2026-04-26 19:52:59 -0700

    overlays: github-copilot-cli 1.0.35 -> 1.0.36

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,7 @@
 
   modifications = final: prev: {
     github-copilot-cli = prev.github-copilot-cli.overrideAttrs (old: rec {
-      version = "1.0.35";
+      version = "1.0.36";
       src = final.fetchurl {
         url = "https://github.com/github/copilot-cli/releases/download/v${version}/${
           {
@@ -15,8 +15,8 @@
         }.tar.gz";
         hash =
           {
-            x86_64-linux = "sha256-H6fM9Ie8f69PvdgP3h3oknF1kNZg0Y1YTQuHgDgCZ4Y=";
-            aarch64-linux = "sha256-wT9Q4EHbck0A/+krCniha+CvY523LYPtmmLr8dK3ZrM=";
+            x86_64-linux = "sha256-m4oAqmFA4LbrAkUmL30/i9sXCi54Jgms3z0ga7wntDE=";
+            aarch64-linux = "sha256-/FYLOxnbVhvEN5p+e+hT+KStG4/AwQlVlVORSnN9xRM=";
           }
           .${final.stdenv.hostPlatform.system}
             or (throw "Unsupported system: ${final.stdenv.hostPlatform.system}");
