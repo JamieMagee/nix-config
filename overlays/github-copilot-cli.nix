69979f31e7ee1797bc33bf335e63cea505d505bd
Jamie Magee <jamie.magee@gmail.com>
2026-06-04 13:31:45 -0700

    overlays: github-copilot-cli 1.0.58 -> 1.0.59

--- overlays/github-copilot-cli.nix
+++ overlays/github-copilot-cli.nix
@@ -1,10 +1,10 @@
 final: prev: {
   github-copilot-cli = prev.github-copilot-cli.overrideAttrs (old: rec {
-    version = "1.0.58";
+    version = "1.0.59";
 
     src = final.fetchurl {
       url = "https://github.com/github/copilot-cli/releases/download/v${version}/github-copilot-${version}.tgz";
-      hash = "sha256-RvmvEEDVJmQ1LF56E5oDe8UNXKXIEcdpv0og79dbNDc=";
+      hash = "sha256-ekMkKJSBVwd7EsaB1L5xxI322r4HMB4A3ehm4cE5dTE=";
     };
 
     # musl libc deps that upstream's ignore list doesn't cover.
