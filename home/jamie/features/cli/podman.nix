a07ab3b1c723f2f2a0016c14701123623fc81365
Jamie Magee <jamie.magee@gmail.com>
2026-09-11 22:43:21 -0700

    podman: autoprune

--- home/jamie/features/cli/podman.nix
+++ home/jamie/features/cli/podman.nix
@@ -1,5 +1,29 @@
+{ config, lib, ... }:
 {
   services.podman = {
     enable = true;
+  };
+
+  systemd.user = {
+    services.podman-prune = {
+      Unit = {
+        Description = "Prune Podman resources";
+      };
+
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${lib.getExe config.services.podman.package} system prune --all --volumes --force";
+      };
+    };
+
+    timers.podman-prune = {
+      Unit.Description = "Prune Podman resources";
+      Timer = {
+        OnCalendar = "weekly";
+        Persistent = true;
+        RandomizedDelaySec = 1800;
+      };
+      Install.WantedBy = [ "timers.target" ];
+    };
   };
 }
